7ae06fe2e16db90cbfe31100084f2ffce7d54b73
cyrille prudhomme <cyrille.prudhomme@gmail.com>
2025-06-03 14:23:22 +0200

    save/load model sur bucket + param bucket

--- celebtwin/ml_logic/registry.py
+++ celebtwin/ml_logic/registry.py
@@ -47,6 +47,14 @@ def save_model(model: keras.Model = None) -> None:
 
     print("✅ Model saved locally")
 
+    if MODEL_TARGET == "gcs":
+        model_filename = model_path.split("/")[-1] # e.g. "20230208-161047.h5" for instance
+        client = storage.Client()
+        bucket = client.bucket(BUCKET_NAME)
+        blob = bucket.blob(f"models/{model_filename}")
+        blob.upload_from_filename(model_path)
+        print("✅ Model saved to GCS")
+
     return None
 
 
@@ -80,3 +88,24 @@ def load_model(stage="Production") -> keras.Model:
         print("✅ Model loaded from local disk")
 
         return latest_model
+
+    elif MODEL_TARGET == "gcs":
+        print(Fore.BLUE + f"\nLoad latest model from GCS..." + Style.RESET_ALL)
+
+        client = storage.Client()
+        blobs = list(client.get_bucket(BUCKET_NAME).list_blobs(prefix="model"))
+
+        try:
+            latest_blob = max(blobs, key=lambda x: x.updated)
+            latest_model_path_to_save = os.path.join(LOCAL_REGISTRY_PATH, latest_blob.name)
+            latest_blob.download_to_filename(latest_model_path_to_save)
+
+            latest_model = keras.models.load_model(latest_model_path_to_save)
+
+            print("✅ Latest model downloaded from cloud storage")
+
+            return latest_model
+        except:
+            print(f"\n❌ No model found in GCS bucket {BUCKET_NAME}")
+
+            return None
