c49a79fc5544f725decfed85646f5ed8cd7aaa96
David Allouche <david@allouche.net>
2025-06-10 14:26:52 +0200

    celebtwin.utils: Add post_mortem decorator for interactive debugging.

--- celebtwin/utils.py
+++ celebtwin/utils.py
@@ -1,5 +1,16 @@
-from celebtwin.params import *
+import sys
+import traceback
+
+import pdb
+from decorator import decorator
 
 
-def func():
-    pass
+@decorator
+def post_mortem(func, *args, **kwargs):
+    try:
+        return func(*args, **kwargs)
+    except Exception:
+        exc_type, exc_value, exc_tb = sys.exc_info()
+        traceback.print_exc()
+        pdb.post_mortem(exc_tb)
+        raise
